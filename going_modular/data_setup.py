73c5a5295bf763e597716f425635a4d7086c22fc
rudgh1999 <dhkscjsrudgh@gmail.com>
2024-02-03 22:36:21 +0900

    haha

--- going_modular/data_setup.py
+++ going_modular/data_setup.py
@@ -15,9 +15,44 @@ Contains functionality for creating PyTorch DataLoaders for
 image classification data.
 """
 import os
-
+import zipfile 
+from pathlib import Path 
+import requests 
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
+
+def download_data(source:str,
+                  destination:str,
+                  remove_source:bool=True) -> Path:
+  # Setup path to data folder 
+  data_path = Path("data/")
+  image_path = data_path / destination 
+
+  # If the image folder doesn't exist, download it and prepare it ...
+  if image_path.is_dir():
+    print(f"[INFO] {image_path} directory exists, skipping download.")
+  else:
+    print(f"[INFO] Did not find {image_path} directory, creating one.")
+    image_path.mkdir(parents=True, exist_ok= True)
+
+    # Download pizza, steak, sushi data
+    target_file = Path(source).name
+    with open(data_path / target_file, "wb") as f:
+      request = requests.get(source)
+      print(f"[INFO] Downloading {target_file} from {source}...")
+      f.write(request.content)
+    
+    # Unzip pizza, steak, sushi data 
+    with zipfile.ZipFile(data_path / target_file, "r") as zip_ref:
+      print(f"[INFO] Unzipping {target_file} data...")
+      zip_ref.extractall(image_path)
+
+      if remove_source:
+        os.remove(data_path / target_file)
+  return image_path
+
+
+
 
 NUM_WORKERS = os.cpu_count()
 
@@ -76,3 +111,5 @@ def create_dataloaders(
 
   return train_dataloader, test_dataloader, class_names
 
+
+# %%
