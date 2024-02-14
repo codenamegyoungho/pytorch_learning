73accc9188f848ee892bad39862d3875b8c77a6f
rudgh1999 <dhkscjsrudgh@gmail.com>
2024-02-14 20:19:06 +0900

    chapter8_firstc

--- going_modular/utils.py
+++ going_modular/utils.py
@@ -33,3 +33,77 @@ def save_model(model: torch.nn.Module,
   print(f"[INFO] Saving model to: {model_save_path}")
   torch.save(obj=model.state_dict(),
              f=model_save_path)
+
+import os  
+import zipfile 
+from pathlib import Path 
+import requests 
+
+def download_data(source:str,
+                  destination:str,
+                  remove_source: bool = True) -> Path:
+    # Setup path to data folder
+    data_path = Path("data/")
+    image_path = data_path / destination 
+
+    # If the image folder doesn't exist, download it and prepare it...
+    if image_path.is_dir():
+        print(f"[INFO] {image_path} directory exists, skipping download.")
+    else:
+        print(f"[INFO] Did not find {image_path} directory, creating one.")
+        image_path.mkdir(parents=True, exist_ok=True)
+
+        # Download pizza, steak, sushi data 
+        target_file = Path(source).name
+        with open(data_path / target_file, "wb") as f:
+            request = requests.get(source)
+            print(f"[INFO] Downloading {target_file} from {source}...")
+            f.write(request.content)
+        
+        # Unzip pizza, steak , sushi data 
+        with zipfile.ZipFile(data_path / target_file, "r") as zip_ref:
+            print(f"[INFO] Unzipping {target_file} data...")
+            zip_ref.extractall(image_path)
+
+        # Remove .zip file 
+            if remove_source:
+                os.remove(data_path / target_file)
+    return image_path
+
+def set_seeds(seed: int=42):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+
+
+from typing import Dict,List
+import matplotlib.pyplot as plt
+def plot_loss_curves(results : Dict[str, List[float]]):
+    # Get the loss values of the results dictionary (training and test)
+    loss = results['train_loss']
+    test_loss = results['test_loss']
+
+    # Get the accuracy values of the results dictionary (training and test)
+    accuracy = results['train_acc']
+    test_accuracy = results['test_acc']
+
+    # Figure out how many epochs there were 
+    epochs = range(len(results['train_loss']))
+
+    # Setup a plot 
+    plt.figure(figsize = (15,7))
+
+    # Plot loss 
+    plt.subplot(1,2,1)
+    plt.plot(epochs, loss, label = 'train_loss')
+    plt.plot(epochs, test_loss, label = 'test_loss')
+    plt.title('Loss')
+    plt.xlabel('Epochs')
+    plt.legend()
+
+    # Plot accuracy 
+    plt.subplot(1,2,2)
+    plt.plot(epochs, accuracy, label = 'train_accuracy')
+    plt.plot(epochs, test_accuracy , label = 'test_accuracy')
+    plt.title('Accuracy')
+    plt.xlabel('Epochs')
+    plt.legend();
